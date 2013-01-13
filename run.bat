f400f17eed16edb824b8d761c44277b2e628fcb6
adorsk <adorsk>
2013-01-13 10:35:42 -0500

    jython distributable project

---
+++ run.bat
@@ -0,0 +1,38 @@
+@echo off
+rem -------------------
+rem SETTINGS
+rem Change settings here.
+rem -------------------
+
+rem Max memory program can use. In some cases seting this
+rem to a higher value can improve speed.
+set MAX_MEM="512m"
+
+rem -------------------
+rem CORE
+rem Don't change stuff below unless
+rem you know what you're doing...
+rem -------------------
+
+rem If running in Take Command (4NT), force to run in cmd.exe
+if not "%@eval[2+2]" == "4" goto normalstart
+cmd /C %0 %*
+goto finish
+
+:normalstart
+set _PERCENT=%%
+set _EXCLAMATION=!
+setlocal enabledelayedexpansion
+
+set JYTHON_CMD="%CD%\.jython\jython.bat"
+set JAVA_LIB="%CD%\.lib\java"
+set PYTHON_PATH=
+
+for /R "%CD%\.lib\java" %%a in (*.jar) do (
+    set PYTHON_PATH=!PYTHON_PATH!;%%a
+    set CLASSPATH=!CLASSPATH!;%%a
+)
+
+echo starting...
+
+%JYTHON_CMD% -J-Xmx%MAX_MEM% -Dpython.path="%PYTHON_PATH%;%CD%\.lib\python" -m entrypoint
---
+++ run.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+echo "starting..."
+
+# -------------------
+# SETTINGS
+# Change settings here.
+# -------------------
+
+# Max memory program can use. In some cases seting this
+# to a higher value can improve speed.
+MAX_MEM="512m"
+
+# -------------------
+# CORE
+# Don't change stuff below unless
+# you know what you're doing...
+# -------------------
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+JYTHON_CMD="$DIR/.jython/jython"
+JAVA_LIBS="$DIR/.lib/java/*.jar"
+PYTHON_PATH=""
+
+for lib in $JAVA_LIBS; do 
+  PYTHON_PATH="$PYTHON_PATH:$lib"
+  CLASSPATH="$CLASSPATH:$lib"
+done
+export CLASSPATH
+
+$JYTHON_CMD -J-Xmx$MAX_MEM -Dpython.path="$PYTHON_PATH:$DIR/.lib/python" -m entrypoint
